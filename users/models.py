0bbf40d9790bc06246c82c3bd34ad9340ea84d11
guidejet <guidejet@gmail.com>
2025-02-05 12:31:03 +0500

    fir

---
+++ users/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class RolesUser(models.Model):
+    role_name = models.CharField(max_length=120)
+    description = models.TextField()
+    def __str__(self):
+        return self.role_name
+
+
+
+# class CustomUser(AbstractUser):
+#     status = models.CharField(max_length=50, null=True, blank=True)  # Статус пользователя
+#     position = models.ForeignKey("Positions", on_delete=models.SET_NULL, null=True)  # Связь с должностью
+#     date_of_birth = models.DateField(null=True, blank=True)  # Дата рождения
+#     address = models.TextField(null=True, blank=True)  # Адрес
+#     department = models.ForeignKey("Department", on_delete=models.SET_NULL, null=True)  # Связь с отделом
+#     phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)  # Телефон
+#     telegram_id = models.CharField(max_length=20, unique=True, null=True, blank=True) #telegrma
+#     is_verified = models.BooleanField(default=False)  # Подтвержден ли пользователь
+#     on_vacation = models.BooleanField(default=False)  # В отпуске ли пользователь
+#     company = models.CharField(max_length=255, null=True, blank=True)  # Название компании
+#
+#     class Meta:
+#         verbose_name = "Пользователь"
+#         verbose_name_plural = "Пользователи"
+
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=120)
+    has_admin = models.BooleanField(default=False)
+    def __str__(self):
+        return self.company_name
+
+class Positions(models.Model):
+    position_name = models.CharField(max_length=120)
+    def __str__(self):
+        return self.position_name
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=120)
+    department_head = models.CharField(max_length=120)
+    def __str__(self):
+        return self.department_name
+
+
+
+
+
+
