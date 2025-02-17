bc576e410cd4b21c5475cc3024e0a5fb129d965a
guidejet <guidejet@gmail.com>
2025-02-17 12:13:20 +0500

    fix: django secret key changed

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#ij%b$n#j-ymxk4nyg_cnn$!r#3n)q9!&uq+ao9u6idzt(9-('
+SECRET_KEY = env('DJSECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
