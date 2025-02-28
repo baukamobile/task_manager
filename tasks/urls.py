733aa96e3437cdc34f04c1156df1d6e0c5c46889
baukamobile <newnewernewest123@gmail.com>
2025-02-28 16:38:09 +0500

    using modelviewset instead generics

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,20 +1,13 @@
-from django.urls import path
+from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from tasks.views import (TaskListView, StatusListView,
-                         TaskDetailView,StatusDetailView,
-                         TaskAddView,StatusAddView,ProjectListView,ProjectAddView)
+from tasks.views import (TaskViewSet,ProjectViewSet,StatusViewSet)
 router = DefaultRouter()
-# router.register(r'status',StatusListView),
-# router.register(r'tasks',TaskListView),
+router.register(r'status',StatusViewSet),
+router.register(r'tasks',TaskViewSet),
+router.register(r'projects',ProjectViewSet)
 urlpatterns = [
-    path('tasks/', TaskListView.as_view(), name='tasks'),
-    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='tasks'),
-    path('status/', StatusListView.as_view(), name='status'),
-    path('status/<int:pk>/', StatusDetailView.as_view(), name='status'),
-    path('tasks/add/',TaskAddView.as_view(), name='taskadd'),
-    path('status/add/', StatusAddView.as_view(), name='statusadd'),
-    path('project/', ProjectListView.as_view(), name='projects'),
-    path('project/add/', ProjectAddView.as_view(), name='projectsadd'),
+    path('',include(router.urls)),
+
 
     # path('',taskpage),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView,RetrieveAPIView,RetrieveUpdateAPIView,CreateAPIView
+from rest_framework.generics import ListAPIView,RetrieveAPIView,RetrieveUpdateAPIView,CreateAPIView,DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -7,36 +8,36 @@ from tasks.models import Task, Status, Projects
 from tasks.serializers import TaskSerializer, StatusSerializer,ProjectSerializer
 
 
-class TaskListView(ListAPIView):  # Отдаёт список всех задач
+class TaskViewSet(ModelViewSet):  # Отдаёт список всех задач
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-class TaskDetailView(RetrieveUpdateAPIView):  # Отдаёт конкретную задачу
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+# class TaskDetailView(RetrieveUpdateAPIView):  # Отдаёт конкретную задачу
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#
+# class TaskAddView(CreateAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
 
-class TaskAddView(CreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-class StatusListView(ListAPIView):
+class StatusViewSet(ModelViewSet):
     # permission_classes = [IsAdminUser]
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
-class StatusDetailView(RetrieveUpdateAPIView):
-    # permission_classes = [IsAdminUser]
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
+# class StatusDetailView(RetrieveUpdateAPIView):
+#     # permission_classes = [IsAdminUser]
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
 
-class StatusAddView(CreateAPIView):
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
+# class StatusAddView(CreateAPIView):
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
 
-class ProjectListView(ListAPIView):
+class ProjectViewSet(ModelViewSet):
     queryset = Projects.objects.all()
     serializer_class = ProjectSerializer
 
-class ProjectAddView(CreateAPIView):
-    queryset = Projects.objects.all()
-    serializer_class = ProjectSerializer
+# class ProjectAddView(CreateAPIView):
+#     queryset = Projects.objects.all()
+#     serializer_class = ProjectSerializer
