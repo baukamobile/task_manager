e9321429269bee4001796b412a8c02054c08aafb
bauyrzhan <newnewernewest123@gmail.com>
2025-03-01 21:04:11 +0500

    tests tasks.project model

--- tasks/tests.py
+++ tasks/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from django.utils.timezone import now
-from tasks.views import *
 import datetime
 from .models import Task, Projects, Status, Priority, Department
 
@@ -11,8 +10,7 @@ User = get_user_model()
 
 class TaskModelTest(TestCase):
     def setUp(self):
-        """Создаем тестовые данные перед каждым тестом"""
-        self.user = User.objects.create(first_name="testuser")
+        self.user = User.objects.create( first_name="testuser")
         self.department = Department.objects.create(department_name="IT")
 
         self.project = Projects.objects.create(
@@ -37,18 +35,16 @@ class TaskModelTest(TestCase):
         )
 
     def test_task_creation(self):
-        """Проверяем, что задача создается"""
         self.assertEqual(Task.objects.count(), 1)
         self.assertEqual(self.task.task_name, "Test Task")
         self.assertEqual(self.task.projects, self.project)
 
     def test_task_str(self):
-        """Проверяем, что __str__ выводит название задачи"""
         self.assertEqual(str(self.task), "Test Task")
+
 
 class TaskApiTest(APITestCase):
     def setUp(self):
-        """Создаем тестовые данные перед каждым тестом"""
         self.user = User.objects.create(first_name="testuser")
         self.department = Department.objects.create(department_name="IT")
 
@@ -72,24 +68,42 @@ class TaskApiTest(APITestCase):
             department=self.department,
             end_date=self.end_date,
         )
+
     def test_task_get_list(self):
-        response = self.client.get('/tasks/tasks/')  # Убедись, что URL правильный
-        print("📢 Ответ API:", response.json())  # Посмотреть, что реально возвращается
+        response = self.client.get('/tasks/tasks/')
+        print("📢 Ответ API:", response.json())
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 1)  # Теперь в базе 1 задача
+        self.assertEqual(len(response.json()), 1)
 
 
-class StatusApitTest(APITestCase):
+class StatusApiTest(APITestCase):
     def setUp(self):
-        self.user = User.objects.create(first_name="testuser")
-        self.status_name = Status.objects.create(status_name='to delete',user=self.user)
+        self.user = User.objects.create( first_name="testuser")
+        self.status = Status.objects.create(status_name='to delete', user=self.user)
 
     def test_get_status_list(self):
         print('status test начинается')
         response = self.client.get('/tasks/status/')
         print("📢 Status API:", response.json())
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
 
-        self.assertEqual(len(response.data),1)
-        print("📢 Status API:", response.json())
 
+class ProjectsApiTestApi(APITestCase):
+    def setUp(self):
+        self.assigned = User.objects.create(first_name='Some User')
+        self.department = Department.objects.create(department_name="IT")
+        self.project = Projects.objects.create(
+            project_name="Some Project",
+            description="Some description",
+            department=self.department,
+            assigned= self.assigned,
+            start_date=now() + datetime.timedelta(days=8),
+            end_date=now() + datetime.timedelta(days=20),
+        )
+
+    def test_get_projects_list(self):
+        response = self.client.get('/tasks/projects/')
+        print("📢 Проекты API:", response.json())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
