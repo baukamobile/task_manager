3d01d490f5b7ff69939f6f6ce3c227ec2344871d
bauyrzhan <newnewernewest123@gmail.com>
2025-03-02 21:55:35 +0500

    fix: url name changed

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from users.views import (UserViewSet, RegisterView,
                          LoginView, LogoutView,
                          PositionsViewSet,RolesViewSet,
-                         CompanyViewSet,Dpa)
+                         CompanyViewSet,DepartmentViewSet)
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -10,12 +10,10 @@ router.register(r'users',UserViewSet),
 router.register(r'positions',PositionsViewSet),
 router.register(r'roles',RolesViewSet),
 router.register(r'company',CompanyViewSet)
-router.register(r'department',DepartmentSerializer)
+router.register(r'department',DepartmentViewSet)
+
 urlpatterns = [
     path('',include(router.urls)),
-    # router.register(r'status',UserViewSet),
-    # path('admin/', admin.site.urls),
-    # path('api/users/', UserList.as_view()),
     path('api/register/', RegisterView.as_view()),
     path('api/login/',LoginView.as_view()),
     path('api/logout/',LogoutView.as_view()),
