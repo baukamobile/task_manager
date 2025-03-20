e8d096a5acb19990d0eab6de2429561f9b4a50c8
baukamobile <newnewernewest123@gmail.com>
2025-03-20 16:36:01 +0500

    fix: send mail works with celery

--- taskmanager/celery.py
+++ taskmanager/celery.py
@@ -1,12 +1,12 @@
 import os
-from celery import Celery
+from celery import Celery # Importing Celery from the celery package
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings')
-
-app = Celery('taskmanager')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-app.conf.update(
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings') # Set the settings  module  to the settings     module
+app = Celery('taskmanager') # Application
+app.config_from_object('django.conf:settings', namespace='CELERY') # Load the configuration from the settings module
+app.autodiscover_tasks(['tasks','users','event_calendar']) # Enable
+app.conf.update( # Additional settings
     worker_concurrency=1,  # Ограничиваем количество воркеров (Windows не любит много процессов)
     worker_prefetch_multiplier=1,  # Минимизируем загрузку процессов
 )
+# celery -A taskmanager worker --pool=solo --loglevel=info
---
+++ users/tasks.py
@@ -0,0 +1,23 @@
+from celery import shared_task
+from django.core.mail import send_mail
+import environ
+from pathlib import Path
+env = environ.Env()
+environ.Env.read_env()
+# @shared-task
+# def send_email()
+
+@shared_task
+def send_mail_message(user_email):
+    send_mail(
+        subject='Добро пожаловать!',
+        message='Спасибо за регистрацию! Администратор подтвердит ваш email.',
+        from_email='bauyrzanbakbergen87@gmail.com',  # Проверь, что email правильный
+        recipient_list=[user_email],  # Должен быть список
+        fail_silently=False,
+    )
+    return f'Email sent to {user_email}'
+
+
+
+
