a7fecfc2235e0000156db2bae27628d101933c8f
baukamobile <newnewernewest123@gmail.com>
2025-04-11 17:34:46 +0500

    nothign

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -52,3 +52,4 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # Медиафайлы (только в DEBUG)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
