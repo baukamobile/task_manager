99c3611d4f8bcf7506e76e941a8f2d6bc24f7ed3
baukamobile <newnewernewest123@gmail.com>
2025-04-23 16:14:41 +0500

    n

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -19,7 +19,7 @@ env = environ.Env()
 environ.Env.read_env()
 # from django.core.management.utils import get_random_secret_key
 #
-# print(get_random_secret_key())
+# print(get_random_secret_key()) ..
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
