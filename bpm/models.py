a2bb904d836d430205519324f53b47f2cb49723f
bauyrzhan <newnewernewest123@gmail.com>
2025-05-07 21:14:22 +0500

    added is_complete field

--- bpm/models.py
+++ bpm/models.py
@@ -67,6 +67,8 @@ class Task(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     completed_at = models.DateTimeField(null=True,blank=True)
     bpmn_task_id = models.CharField(max_length=100,null=True)
+    is_completed = models.BooleanField(default=False)
+
     def __str__(self):
         return self.status
     def is_overdue(self):
